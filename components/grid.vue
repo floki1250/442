175342b6c343495151a781e7d0c39a0e9440e716
Adem <37814393+floki1250@users.noreply.github.com>
2022-06-25 12:04:22 +0100

    442

---
+++ components/grid.vue
@@ -0,0 +1,120 @@
+<template>
+  <client-only class="Field">
+    <grid-layout
+      :layout.sync="layout"
+      :col-num="30"
+      :row-height="30"
+      :is-draggable="draggable"
+      :is-resizable="false"
+      :vertical-compact="false"
+      :use-css-transforms="false"
+    >
+      <grid-item
+        v-for="item in layout"
+        :key="item"
+        :static="item.static"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+      >
+        <span class="text">{{ itemTitle(item) }}</span>
+      </grid-item>
+    </grid-layout>
+  </client-only>
+</template>
+
+<script>
+import ClientOnly from 'vue-client-only'
+
+export default {
+  components: {
+    ClientOnly,
+  },
+  data() {
+    return {
+      layout: [
+        { x: 2, y: 3, w: 2, h: 2, i: '1', static: false },
+        { x: 4, y: 0, w: 2, h: 2, i: '2', static: false },
+        { x: 6, y: 0, w: 2, h: 2, i: '3', static: false },
+        { x: 8, y: 0, w: 2, h: 2, i: '4', static: false },
+        { x: 10, y: 0, w: 2, h: 2, i: '5', static: false },
+        { x: 0, y: 5, w: 2, h: 2, i: '6', static: false },
+        { x: 2, y: 5, w: 2, h: 2, i: '7', static: false },
+        { x: 4, y: 5, w: 2, h: 2, i: '8', static: false },
+        { x: 6, y: 3, w: 2, h: 2, i: '9', static: false },
+        { x: 8, y: 4, w: 2, h: 2, i: '10', static: false },
+        { x: 10, y: 1, w: 2, h: 2, i: '11', static: false },
+      ],
+
+      index: 0,
+    }
+  },
+  methods: {
+    itemTitle(item) {
+      let result = item.i
+      if (item.static) {
+        result += ' - Static'
+      }
+      return result
+    },
+  },
+}
+</script>
+
+<style scoped>
+.vue-grid-layout {
+}
+.vue-grid-item {
+  border-radius: 100%;
+}
+.vue-grid-item:not(.vue-grid-placeholder) {
+  background: rgb(73, 47, 223);
+
+  border: 1px solid black;
+}
+.vue-grid-item .resizing {
+  opacity: 0.1;
+}
+.vue-grid-item .static {
+  background: #cce;
+}
+.vue-grid-item .text {
+  font-size: 24px;
+  text-align: center;
+  position: relative;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  height: 100%;
+  width: 100%;
+}
+.vue-grid-item .no-drag {
+  height: 100%;
+  width: 100%;
+}
+.vue-grid-item .minMax {
+  font-size: 12px;
+}
+.vue-grid-item .add {
+  cursor: pointer;
+}
+.vue-draggable-handle {
+  position: relative;
+  width: 20px;
+  height: 20px;
+  top: 0;
+  left: 0;
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>")
+    no-repeat;
+  background-position: bottom right;
+  padding: 0 8px 8px 0;
+  background-repeat: no-repeat;
+  background-origin: content-box;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="layout">
+    <div>
+      <div class="header">
+        <img
+          src="../assets/442Logo.svg"
+          alt="442"
+          class="logo"
+          style="width: 100px; padding: 10px"
+        />
+      </div>
+      <div class="columns">
+        <div class="column is-three-quarters">
+          <fullscreen v-model="fullscreen">
+            <div style="position: relative">
+              <div class="bg">
+                <img src="../assets/field.jpg" alt="" class="Field" />
+              </div>
+              <div class="players">
+                <Grid></Grid>
+              </div>
+            </div>
+          </fullscreen>
+        </div>
+
+        <div class="column settings">
+          <div class="columns is-mobile">
+            <div class="column container">
+              <img
+                src="https://apiv2.allsportsapi.com/logo/96_juventus.jpg"
+                alt=""
+                width="100px"
+              />
+            </div>
+            <div class="column container">
+              <img
+                src="https://apiv2.allsportsapi.com/logo/96_juventus.jpg"
+                alt=""
+                width="100px"
+              />
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">0</div>
+            <div class="column">0</div>
+          </div>
+          <button type="button" @click="toggle" class="button">
+            Toggle Fullscreen
+          </button>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <p>hfsdfffffff</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueFullscreen from 'vue-fullscreen'
+import Vue from 'vue'
+import Grid from '../components/grid.vue'
+Vue.use(VueFullscreen)
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      fullscreen: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.fullscreen = !this.fullscreen
+    },
+  },
+  components: { Grid },
+}
+</script>
