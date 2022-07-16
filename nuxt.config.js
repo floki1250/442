bd040ccbc6acc7399fcf2af839294cbed359d040
Adem <37814393+floki1250@users.noreply.github.com>
2022-07-16 12:09:31 +0100

    Add SCSS Buefy Variables 😁

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/main.scss', 'bulma'],
+  css: ['@/assets/style/main.scss', 'bulma', '@/assets/style/variables.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~/plugins/vue-grid', ssr: false }],
@@ -45,7 +45,9 @@ export default {
       },
     ],
   ],
-
+  buefy: {
+    css: false,
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
